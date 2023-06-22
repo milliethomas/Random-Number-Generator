d5cf63d533ed04b3935a45227376b3e20a55fa82
BuildTools <unconfigured@null.spigotmc.org>
2023-06-22 22:04:38 +0100

    added changes

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', function(){
     var generateBtn = document.getElementById('generateBtn');
 
     generateBtn.addEventListener('click', function() {
-        var rand = Math.floor(Math.random() * 100) + 1;
         var num = document.getElementById('num');
-        num.textContent = rand;
+        var lower = document.getElementById('lower').value;
+        var upper = document.getElementById('upper').value;
+        console.log(lower)
+        num.textContent = getRandom(lower, upper);
     });
 });
+
+function getRandom(lower, upper){
+    return Math.floor(Math.random() * (upper - lower + 2) + lower);
+}
